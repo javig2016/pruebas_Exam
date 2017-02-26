1cabac53bf03236e2eeda4d9e3007c81de7fc5ba
javig2016 <javiergonzalez08@ono.com>
2017-02-26 20:41:40 +0100

    Add files via upload

--- js/questions.js
+++ js/questions.js
@@ -1,14 +1,6 @@
 var formElement=null;
 var answer1_txt=null;
-var answer2_rad=null;
-var answer3_txt=null;
-var answer4_sel=null;
-var answer5_rad=null;
-var answer6_sel=null;
-var answer7_check = [];
-var answer8_mul= [];
-var answer9_check = [];
-var answer10_mul= [];
+
 var nota = 0;  //nota de la prueba sobre 10 puntos (tenemos 10  preguntas)
 
 //**************************************************************************************************** 
@@ -43,16 +35,17 @@ window.onload = function(){
 // xmlDOC es el documento leido XML. 
 function gestionarXml(datosXml){
   //Parse XML a  xmlDoc
-var xmlDoc = datosXML.responseXML;
+  var xmlDoc = datosXML.responseXML;
   // Llamar a pregunta XML
-  var tituloInput; //pregunta_XML
+  var pregunta_XML;
   // Elemento HTML donde va la pregunta
   var pregunta_HTML; //pregunta_HTML
  
 
   //Pregunta 1 tipo texto
-  tituloInput = xmlDoc.getElementsByTagName("title")[0].innerHTML;
-  ponerDatosInputHtml(tituloInput);
+  pregunta_XML = xmlDoc.getElementsByTagName("title")[0].innerHTML;
+  pregunta_HMTL = document.getElementById("p001");
+  ponerDatosInputHtml(pregunta_XML, pregunta_HMTL);
   answer1_txt = xmlDoc.getElementById("preg001").getElementsByTagName("answer")[0].innerHTML;
 
 
@@ -64,40 +57,21 @@ var xmlDoc = datosXML.responseXML;
 //****************************************************************************************************
 //implementación de la corrección
 
-function corregirTexto(valor, correcto, mensajeAcierto, mensajeFallo){
-  if (valor.toLowerCase() == correcto.toLowerCase()) {
-   darRespuestaHtml(mensajeAcierto);
-   nota +=1;
-  }
-  else {
-   darRespuestaHtml(mensajeFallo);
-  }
-}
 
+function inicializar()
+{
+  document.getElementById('').innerHTML = "";
+  nota=0;
+}
 
 
 //****************************************************************************************************
 // poner los datos recibios en el HTML
-function ponerDatosInputHtml(pregunta){
-  document.getElementbyID("tituloInput").innerHTML= pregunta;
+function ponerDatosInputHtml(objeto_HTML, objeto_XML){
+  objeto_HMTL.innerHTML = objeto_XML;
 }
 
 
 
 //****************************************************************************************************
 //Gestionar la presentación de las respuestas
-function darRespuestaHtml(solucion){
- var p = document.createElement("p");
- var node = document.createTextNode(solucion);
- p.appendChild(node);
- document.getElementById('resultadosDiv').appendChild(p);
-}
-
-function presentarNota(){
-   darRespuestaHtml("Nota: "+nota+" puntos sobre 10");
-}
-
-function inicializar(){
-   document.getElementById('resultadosDiv').innerHTML = "";
-   nota=0.0;
-}
