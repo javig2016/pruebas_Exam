6bb271380062d12efaf1a779f849bff733df3be0
javig2016 <javiergonzalez08@ono.com>
2017-02-26 21:56:37 +0100

    Add files via upload

--- js/questions.js
+++ js/questions.js
@@ -47,58 +47,7 @@ function gestionarXml(dadesXml){
 //****************************************************************************************************
 //implementación de la corrección
 
-function corregirNumber(){
-  //Vosotros debéis comparar el texto escrito con el texto que hay en el xml
-  //en este ejemplo hace una comparación de números enteros
-  var s=formElement.elements[0].value;     
-  if (s==respuestatexto) {
-   darRespuestaHtml("P1: Exacto!");
-   nota +=1;
-  }
-  else {
-    if (s>respuestatexto) darRespuestaHtml("P1: Te has pasado");
-    else darRespuestaHtml("P1: Te has quedado corto");
-  }
-}
 
-function corregirSelect(){
-  //Compara el índice seleccionado con el valor del íncide que hay en el xml (<answer>2</answer>)
-  //para implementarlo con type radio, usar value para enumerar las opciones <input type='radio' value='1'>...
-  //luego comparar ese value con el value guardado en answer
-  var sel = formElement.elements[1];  
-  if (sel.selectedIndex==respuestaSelect) {
-   darRespuestaHtml("P2: Correcto");
-   nota +=1;
-  }
-  else darRespuestaHtml("P2: Incorrecto");
-}
-
-//Si necesitáis ayuda para hacer un corregirRadio() decirlo, lo ideal es que a podáis construirla modificando corregirCheckbox
-function corregirCheckbox(){
-  //Para cada opción mira si está checkeada, si está checkeada mira si es correcta y lo guarda en un array escorrecta[]
-  var f=formElement;
-  var escorrecta = [];
-  for (i = 0; i < f.color.length; i++) {  //"color" es el nombre asignado a todos los checkbox
-   if (f.color[i].checked) {
-    escorrecta[i]=false;     
-    for (j = 0; j < respuestasCheckbox.length; j++) {
-     if (i==respuestasCheckbox[j]) escorrecta[i]=true;
-    }
-   } 
-  }
-  //Por cada opción que está chequedada, si es correcta sumamos y ponemos mensaje, si no es correcta restamos y ponemos mensaje.
-  for (i = 0; i < f.color.length; i++) {   
-   if (f.color[i].checked) {
-    if (escorrecta[i]) {
-     nota +=1.0/respuestasCheckbox.length;  //dividido por el número de respuestas correctas   
-     darRespuestaHtml("P3: "+i+" correcta");    
-    } else {
-     nota -=1.0/respuestasCheckbox.length;  //dividido por el número de respuestas correctas   
-     darRespuestaHtml("P3: "+i+" incorrecta");
-    }   
-   }
-  }
-}
 
 //****************************************************************************************************
 // poner los datos recibios en el HTML
@@ -107,52 +56,6 @@ function ponerDatosInputHtml(elementoHTML, elementoXML)
   elementoHTML.innerHTML = elementoXML;
 }
 
-function ponerDatosInputHtml2(t){
- document.getElementById("preg003").innerHTML = t;
-}
-
-
-function ponerDatosSelectHtml(t,opt){
-  document.getElementById("tituloSelect").innerHTML=t;
-  var select = document.getElementsByTagName("select")[0];
-  for (i = 0; i < opt.length; i++) { 
-    var option = document.createElement("option");
-    option.text = opt[i];
-    option.value=i+1;
-    select.options.add(option);
- }  
-}
-
-function ponerDatosCheckboxHtml(t,opt){
- var checkboxContainer=document.getElementById('checkboxDiv');
- document.getElementById('tituloCheckbox').innerHTML = t;
- for (i = 0; i < opt.length; i++) { 
-    var input = document.createElement("input");
-    var label = document.createElement("label");
-    label.innerHTML=opt[i];
-    label.setAttribute("for", "color_"+i);
-    input.type="checkbox";
-    input.name="color";
-    input.id="color_"+i;;    
-    checkboxContainer.appendChild(input);
-    checkboxContainer.appendChild(label);
- }  
-}
 
 //****************************************************************************************************
 //Gestionar la presentación de las respuestas
-function darRespuestaHtml(r){
- var p = document.createElement("p");
- var node = document.createTextNode(r);
- p.appendChild(node);
- document.getElementById('resultadosDiv').appendChild(p);
-}
-
-function presentarNota(){
-   darRespuestaHtml("Nota: "+nota+" puntos sobre 3");
-}
-
-function inicializar(){
-   document.getElementById('resultadosDiv').innerHTML = "";
-   nota=0.0;
-}
